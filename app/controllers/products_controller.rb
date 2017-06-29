7bf03a60a11fc39b8a32b93a59bc15f017fdf462
Ko-heiIto <hawai5603@gmail.com>
2017-06-29 13:27:19 +0900

    first commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,21 @@
 class ProductsController < RankingController
+
+  before_action :authenticate_user!, only: :search
+
+  def index
+    @products = Product.order('id ASC').limit(20)
+  end
+
+  def show
+    # productsテーブルから該当するidの作品情報を取得し@productの変数へ代入する処理を書いて下さい# 問題3ではこのコードは消して新しくコードを書いてください
+    @product = Product.find(params[:id])
+  end
+
+  def search
+    @products = Product.where('title LIKE(?)', "%#{params[:keyword]}%").limit(20)
+  end
+
+  def create
+    
+  end
 end
